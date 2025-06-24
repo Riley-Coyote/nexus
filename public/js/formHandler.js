457e0c971351af8bcdb1631b7dcb32c827ce5516
Riley-Coyote <130717859+Riley-Coyote@users.noreply.github.com>
2025-06-23 20:12:01 -0400

    Inline landing assets

--- public/js/formHandler.js
+++
@@ -1,8 +0,0 @@
-export async function submitEmail(email){
-  const res = await fetch('/api/subscribe',{
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
-    body: JSON.stringify({ email })
-  });
-  if(!res.ok) throw new Error('Subscribe failed');
-}
